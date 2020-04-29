15f05c0578d6c280bc81a3bc0fc054c49c7e1f26
milse113 <milse113@gmail.com>
2020-04-29 11:09:58 -0400

    Forgot circumference of Oval :P

--- src/main/kotlin/com/leafclient/maths/Oval.kt
+++ src/main/kotlin/com/leafclient/maths/Oval.kt
@@ -1,10 +1,14 @@
 package com.leafclient.maths
 
 import com.leafclient.maths.point.Point2
+import kotlin.math.sqrt
 
 class Oval(var major: Double, var minor: Double, var center: Point2) {
     val area: Double
         get() = kotlin.math.PI*major*minor
+
+    val circumference: Double
+        get() = 2*kotlin.math.PI*sqrt((major+minor)/2)
 
     val circle: Circle?
         get() = if(isCircle()) {Circle(major, center)} else {null}
