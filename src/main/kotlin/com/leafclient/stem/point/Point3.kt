c6b912d5d896fa851d6efc661a1986dfc6d11b21
Shyrogan <shyrogancontact@gmail.com>
2020-06-03 17:20:44 +0200

    Adding a point(double, double) and point(double, double, double) method

--- src/main/kotlin/com/leafclient/stem/point/Point3.kt
+++ src/main/kotlin/com/leafclient/stem/point/Point3.kt
@@ -20,8 +20,7 @@ class Point3(var x: Double, var y: Double, var z: Double) {
     operator fun plus(other: Vector3) =
         Point3(
             other.x + x,
-            other.y + y,
-            other.z + z
+            other.y + y, other.z + z
         )
 
     operator fun minus(other: Vector3) =
